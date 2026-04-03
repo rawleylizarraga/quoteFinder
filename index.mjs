eeed6745d423f894cc028b0afc0403812c37a5a7
Rawley Lizarraga <rawleylizarraga@gmail.com>
2026-04-02 19:09:17 -0700

    end of tutorial

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //for Express to get values using POST method
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //setting up database connection pool
 const pool = mysql.createPool({
@@ -20,12 +20,59 @@ const pool = mysql.createPool({
 });
 
 //routes
-app.get('/', (req, res) => {
-   res.render('index')
+app.get('/', async (req, res) => {
+    let sql = `
+    SELECT authorId, firstName, lastName
+    FROM q_authors
+    ORDER BY lastName`;
+    const [rows] = await pool.query(sql);
+    res.render('index', { "authors": rows });
 });
 
-app.get("/dbTest", async(req, res) => {
-   try {
+app.get('/searchByKeyword', async (req, res) => {
+    let userKeyword = req.query.keyword;
+    // console.log(userKeyword);
+    let sql = `
+        SELECT quote, authorId, firstName, lastName
+        FROM q_quotes
+        NATURAL JOIN q_authors
+        WHERE quote LIKE ?`;
+
+    let sqlParams = [`%${userKeyword}%`];
+
+    const [rows] = await pool.query(sql, sqlParams);
+
+    // res.render(rows);
+    res.render("results", { "quotes": rows });
+});
+
+app.get('/searchByAuthor', async (req, res) => {
+    let userAuthorId = req.query.authorId;
+    let sql = `
+        SELECT authorId, firstName, lastName, quote
+        FROM q_quotes
+        NATURAL JOIN q_authors
+        WHERE authorId = ?`;
+
+    let sqlParams = [userAuthorId];
+
+    const [rows] = await pool.query(sql, sqlParams);
+
+    res.render("results", { "quotes": rows });
+});
+
+app.get('/api/author/:id', async (req, res) => {
+    let authorId = req.params.id;
+    let sql = `
+        SELECT *
+        FROM q_authors
+        WHERE authorId = ?`;
+    let [rows] = await pool.query(sql, [authorId]);
+    res.send(rows)
+});
+
+app.get("/dbTest", async (req, res) => {
+    try {
         const [rows] = await pool.query("SELECT CURDATE()");
         res.send(rows);
     } catch (err) {
@@ -34,6 +81,6 @@ app.get("/dbTest", async(req, res) => {
     }
 });//dbTest
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Express server running")
 })
---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+// event listeners
+let authorLinks = document.querySelectorAll("a");
+for (authorLink of authorLinks) {
+    authorLink.addEventListener("click", getAuthorInfo);
+}
+
+// variables
+
+
+// functions
+async function getAuthorInfo() {
+    // alert(this.id);
+
+    var myModal = new bootstrap.Modal(document.getElementById('authorModal'));
+    myModal.show();
+
+    let url = `/api/author/${this.id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+
+    let authorInfo = document.querySelector("#authorInfo");
+    authorInfo.innerHTML = `<h1> ${data[0].firstName} ${data[0].lastName} </h1>`;
+    authorInfo.innerHTML += `<img src="${data[0].portrait}" width="200"> <br>`;
+    authorInfo.innerHTML += `Born: ${data[0].dob.split("T")[0]} <br>`;
+    authorInfo.innerHTML += `Died: ${data[0].dod.split("T")[0]} <br>`;
+    authorInfo.innerHTML += `Profession: ${data[0].profession} <br>`;
+    authorInfo.innerHTML += `Country: ${data[0].country} <br><br>`;
+    authorInfo.innerHTML += `${data[0].biography} <br>`;
+}
